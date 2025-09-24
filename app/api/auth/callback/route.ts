81a2a235283106583721dedd56aa883224bebb17
庄暐 <zhuangwei@zhuangweideMacBook-Air.local>
2025-09-24 15:20:38 +0800

    fix(api): Correctly handle Supabase auth callback

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -2,15 +2,19 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { createSupabaseRouteHandlerClient } from '@/lib/supabase/server'
 
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
   const locale = requestUrl.searchParams.get('locale') ?? 'zh'
   const redirectTo = requestUrl.searchParams.get('redirectTo') ?? `/${locale}/dashboard`
 
-  const supabase = createSupabaseRouteHandlerClient()
+  if (code) {
+    const supabase = createSupabaseRouteHandlerClient()
+    await supabase.auth.exchangeCodeForSession(code)
+  }
 
-  await supabase.auth.exchangeCodeForSession(request)
-
+  // URL to redirect to after sign in process completes
   return NextResponse.redirect(new URL(redirectTo, requestUrl.origin), {
     status: 302
   })
